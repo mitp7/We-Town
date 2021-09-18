0fd04349692e42e33aa1a7832de3116cef0c7be0
David Kim <j626kim@uwaterloo.ca>
2021-09-18 18:33:20 -0400

    added tailwind css support

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,13 +2,14 @@
 import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Home from './pages/home';
+import Login from './pages/login';
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
         <Switch>
-          <Route exact path="/" component={Home}>
+          <Route exact path="/" component={Login}>
           </Route>
         </Switch>
       </BrowserRouter>
