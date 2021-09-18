e25915beab324b12c2252d90b042d53fd3e982d1
David Kim <j626kim@uwaterloo.ca>
2021-09-18 16:45:14 -0400

    setup frontend code

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,9 @@
 const express = require('express')
 const app = express()
-const port = 3000
+const port = 5000
 
-app.get('/', (req, res) => {
+app.get('/api', (req, res) => {
+  console.log('j')
   res.send('Hello World!')
 })
 
