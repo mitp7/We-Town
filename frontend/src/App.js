d6e742864dc9e7f07263d5ec86799f552664011b
Zayed Kherani <Zayedkherani@gmail.com>
2021-09-19 04:26:33 -0400

    //authour: Zayed Kherani; Added Nav Bar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,21 +1,23 @@
-
-import React from 'react';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
-import Home from './pages/home';
-import Login from './pages/login';
+// import React from 'react';
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import Home from "./pages/home";
+import Login from "./pages/login";
+import NavBar from "./components/navBar";
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
         <Switch>
-          <Route exact path="/" component={Login}>
-          </Route>
+          {/* <Route exact path="/" component={Login}> */}
+          <Route exact path="/" component={Login}></Route>
+          <NavBar>
+            <Route path="/home" component={Home}></Route>
+          </NavBar>
         </Switch>
       </BrowserRouter>
     </div>
-
-   );
+  );
 }
 
 export default App;
